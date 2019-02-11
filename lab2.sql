e1479873f08662f25672ad563e075c6eb82538b2
Filip Bergman <fi5731be-s@panter-10.e.lth.lu.se>
2019-02-11 16:51:23 +0100

    lade till objekt

--- lab2.sql
+++ lab2.sql
@@ -22,27 +22,33 @@ CREATE TABLE theaters (
 
 
 CREATE TABLE performances (
-  performance_id	TEXT DEFAULT (lower(hex(randomblob(16)))),
   start_time		TIME,
+  start_date		DATE,
   theater_name		TEXT,
   imdb_key		TEXT,
-  PRIMARY KEY (performance_id),
+  PRIMARY KEY (start_time, start_date, theater_name),
   FOREIGN KEY (theater_name) REFERENCES theaters(theater_name),
   FOREIGN KEY (imdb_key) REFERENCES movies(imdb_key)
 );
 
 
 CREATE TABLE movies (
-  imdb_key		TEXT DEFAULT (lower(hex(randomblob(16)))),
+  imdb_key		TEXT,
   movie_name		TEXT,
   production_year	INT,
   running_time		INT,
   PRIMARY KEY (imdb_key)
 );
 
+CREATE TABLE customers(
+  username		TEXT,
+  full_name		TEXT,
+  password		TEXT,
+  PRIMARY KEY (username)
+);
 
 CREATE TABLE tickets (
-  t_id		TEXT DEFAULT (lower(hex(randomblob(16)))),
+  t_id			TEXT DEFAULT (lower(hex(randomblob(16)))),
   performance_id	TEXT,
   username		TEXT,
   PRIMARY KEY (t_id),
@@ -50,26 +56,32 @@ CREATE TABLE tickets (
   FOREIGN KEY (username) REFERENCES customers(username)
 );
 
+INSERT INTO customers VALUES ("Bertil45", "Bert Jansson", "1234");
+INSERT INTO customers VALUES ("Pelle1337", "Pär Nilsson", "password");
+INSERT INTO customers VALUES ("Kurtinator", "Kurt Kurtsson", "kurtärbäst");
+INSERT INTO customers VALUES ("Nilzzz", "Nils Bo", "zzzlin");
+INSERT INTO customers VALUES ("WalterB", "Walter Andersson", "princess");
 
-CREATE TABLE customers(
-  username		TEXT,
-  full_name		TEXT,
-  password		TEXT,
-  t_id			TEXT,
-  PRIMARY KEY (username),
-  FOREIGN KEY (t_id) REFERENCES tickets(t_id)
-);
-
-
-
+INSERT INTO tickets (performance_id, username) VALUES ("5e6487fa6a1a8efbfafe7b720b62ff56", "WalterB");
+INSERT INTO tickets (performance_id, username) VALUES ("db1a611b5cedf3f27b6f751c6d5df847", "WalterB");
+INSERT INTO tickets (performance_id, username) VALUES ("db1a611b5cedf3f27b6f751c6d5df847", "Nilzzz");
 
 INSERT INTO theaters VALUES ("Filmstaden Lund", 237);
 INSERT INTO theaters VALUES ("Royal Malmö", 501);
+INSERT INTO theaters VALUES ("Filmstaden Uddevalla", 211);
 
 INSERT INTO movies VALUES ("tt0111161", "The Shawshank Redemption", 1994, 144);
 INSERT INTO movies VALUES ("tt0816692", "Interstellar", 2014, 169);
 INSERT INTO movies VALUES ("tt0892769", "How to Train your Dragon", 2010, 98);
+INSERT INTO movies VALUES ("tt2084970", "The Imitation Game", 2014, 114);
+INSERT INTO movies VALUES ("tt1201607", "Harry Potter and the Deathly Hallows: Part 2", 2011, 130);
+INSERT INTO movies VALUES ("tt0167260", "Lord of the Rings: The Return of the King", 2003, 201);
+
+INSERT INTO performances (start_time, start_date, theater_name, imdb_key) VALUES("19:30", "2019-02-16", "Filmstaden Lund", "tt0111161");
+INSERT INTO performances (start_time, start_date, theater_name, imdb_key) VALUES("20:30", "2019-02-16", "Filmstaden Lund", "tt0111161");
+INSERT INTO performances (start_time, start_date, theater_name, imdb_key) VALUES("19:30", "2019-02-14", "Filmstaden Lund", "tt0111161");
 
 
-INSERT INTO performances (start_time, theater_name, imdb_key) VALUES("19:30", "Filmstaden Lund", "tt0111161");
+INSERT INTO performances (start_time, start_date, theater_name, imdb_key) VALUES("20:30", "2019-02-15", "Filmstaden Uddevalla", "tt2084970");
+INSERT INTO performances (start_time, start_date, theater_name, imdb_key) VALUES("19:30", "2019-02-14", "Royal Malmö", "tt0816692");
 
